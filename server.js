6cc4abe524e36d9c3a421b1d93cd3dcce21c8f2b
Darkvinces <darkvinces1423@gmail.com>
2025-12-15 22:14:00 +0700

    Add server.js for Plesk deployment

--- server.js
+++ server.js
@@ -1,32 +1,18 @@
 const { createServer } = require('http')
-require('dotenv').config()
 const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
-const port = process.env.PORT || 3000
-
-// setup next app
-const app = next({ dev, hostname, port })
+const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const port = process.env.PORT || 3000
+
 app.prepare().then(() => {
-    createServer(async (req, res) => {
-        try {
-            const parsedUrl = parse(req.url, true)
-            await handle(req, res, parsedUrl)
-        } catch (err) {
-            console.error('Error occurred handling', req.url, err)
-            res.statusCode = 500
-            res.end('internal server error')
-        }
+    createServer((req, res) => {
+        const parsedUrl = parse(req.url, true)
+        handle(req, res, parsedUrl)
+    }).listen(port, () => {
+        console.log(`> Ready on http://localhost:${port}`)
     })
-        .once('error', (err) => {
-            console.error(err)
-            process.exit(1)
-        })
-        .listen(port, () => {
-            console.log(`> Ready on http://${hostname}:${port}`)
-        })
 })
